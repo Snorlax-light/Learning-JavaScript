350793326ee3dda2d50d8c83579fcaee9186e08e
Snorlax-light <johnsulaiman4@gmail.com>
2025-08-24 14:03:27 -0700

    JS tutorial

---
+++ JS tutorial 2/script.js
@@ -0,0 +1,51 @@
+console.log("Hello World");
+alert("hello World");
+
+
+// automatic declaration
+x = 5;
+y = 12.5;
+z = "Hello World";
+
+// let declaration
+let x = 5;
+let y = 12.5;
+let z = "Hello World";
+
+
+
+// constant
+const a = 5;
+const b = 12.5;
+const c = "Hello World";
+
+
+//var
+var d = 5;
+var e = 12.5;
+var f = "Hello World";
+
+/*------------------------------*/
+
+//string
+let name = "david";
+
+
+// number
+let number = 5;
+
+
+//Boolean
+let isCoding = false;
+
+
+//Undefined
+let nickname;
+
+
+//Null
+let lastName = null;
+
+
+
+
