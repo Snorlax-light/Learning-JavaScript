eee1201ec57b5171fc8890d1cdf7aa3c69651e9c
Snorlax-light <johnsulaiman4@gmail.com>
2025-08-25 06:00:11 -0700

    JavaScript Toturial day 3

---
+++ JS tutorial 3/script.js
@@ -0,0 +1,61 @@
+// converts string to num
+let number1 = "12.5";
+let number2 = "12";
+
+
+// convert. 
+number2 = parseInt(number2);
+
+//alert(number1 + number2 );
+
+number1 = parseFloat(number1);
+//alert(number1);
+
+let x = 5;
+let y = 10;
+
+//alert((x + y) /x);
+
+
+let num1 = 5;
+let num2 = 2;
+
+//add
+        //alert(num1 + num2);
+
+//sub
+        //alert(num1 - num2);
+
+//Multi
+        //alert(num1 * num2);
+
+//div
+        //alert(num1 / num2);
+
+// modulus
+        //alert(num1 % num2);
+
+//exponent
+        //alert(num1 ** num2);
+
+
+let num = 5;
+
+num += 5;
+//alert(num);
+
+
+// create a program that uses variables for 5 subject grades. 
+// Get the avaerage of all the grades and display it. 
+
+let math = 79;
+let science = 80;
+let Ap = 85;
+let PE = 92;
+let Re = 90;
+
+average = (math + science + Ap + PE + Re)/5;
+
+alert( "Average is: " +average);
+
+
