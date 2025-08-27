aa7d99efc2df2f7ca191d454fb6039233aaf12c4
Snorlax-light <johnsulaiman4@gmail.com>
2025-08-27 05:35:07 -0700

    day 4

--- JS tutorial 4/scipt.js
+++ JS tutorial 4/scipt.js
@@ -50,5 +50,5 @@ alert(`
     ${person1}, the friendly neighbor, waved at
     ${person2} as ${pronoun} walked by 
     ${person2}'s house.  ${person2} smiled back and invited  ${person1}
-    in for a cup of ${drink}. `
+    in for a cup of ${drink}.. `
 );
