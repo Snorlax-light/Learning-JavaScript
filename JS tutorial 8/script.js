86fd7f2e9a140d75eda26da4873c06004faf4ee0
Snorlax-light <johnsulaiman4@gmail.com>
2025-09-01 18:29:20 -0700

    switch case

---
+++ JS tutorial 8/script.js
@@ -0,0 +1,64 @@
+//Switch Statement
+
+let difficulty = 5;
+
+switch(difficulty){
+    case 1 :
+        console.log("Easy");
+        break;
+    case 2 : 
+        console.log("Normal");
+        break;
+    case 3 :
+        console.log("Hard");
+        break;
+    default :
+        console.log("Invalid Difficulty");
+        break;
+}
+
+
+let num = 2;
+
+switch(num){
+    case 2 :
+    case 4 :
+        console.log("Even");
+        break;
+    case 3 :
+    case 5 :
+        console.log("Odd");
+        break;
+    default : 
+        break;
+}
+
+
+let day = 1;
+
+switch(day){
+    case 1 :
+        console.log("Monday");
+        break;
+    case 2 :
+        console.log("Tuesday");
+        break;
+    case 3 :
+        console.log("Wednesday");
+        break;
+    case 4 :
+        console.log("Thursday");
+        break;
+    case 5 :
+        console.log("Friday");
+        break;
+    case 6 :
+        console.log("Saturday");
+        break;
+    case 7 :
+        console.log("Sunday");
+        break;
+    default :
+        console.log("Invalid Input");
+        break;
+}
