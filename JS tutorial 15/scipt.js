d508c3fc4fc10ad2d28221c940785c90bd1b81b4
Snorlax-light <johnsulaiman4@gmail.com>
2025-09-19 05:05:25 -0700

    day 15

---
+++ JS tutorial 15/scipt.js
@@ -0,0 +1,34 @@
+// event listeners
+
+//onload
+/*
+function initializedPage(){
+    alert("page loaded!");
+}
+*/
+
+
+/*
+//onclick
+document.getElementById("btn").addEventListener("click", Greetings);
+
+function Greetings()
+{
+    alert("Hello World");
+}
+
+//onmouseover
+function hoverTxt(){
+    let p = document.querySelector("p");
+    p.innerHTML = "You Hovered me!";
+}
+
+*/
+/*
+//onfucos
+function triggerFucos(){
+    let inp = document.querySelector("input");
+    inp.style.background = "black";
+}
+*/
+
